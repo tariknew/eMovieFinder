f56fb2a900d53a60d1c7217af24263970a4fd5b5
tariknew <83304304+tariknew@users.noreply.github.com>
2025-02-01 14:10:15 +0100

    Delete eMovieFinder directory

--- eMovieFinder/eMovieFinder.Model/Dtos/Requests/Order/OrderSalesReportInsertRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eMovieFinder.Model.Dtos.Requests.Order
-{
-    public class OrderSalesReportInsertRequest
-    {
-        public int MovieId { get; set; }
-        public int? Year { get; set; }
-        public int? Month { get; set; }
-    }
-}
--- eMovieFinder/eMovieFinder.Services/Interfaces/IActorService.cs
+++
@@ -1,8 +0,0 @@
-﻿using eMovieFinder.Model.Dtos.Requests.Actor;
-using eMovieFinder.Model.Entities;
-using eMovieFinder.Model.SearchObjects;
-
-namespace eMovieFinder.Services.Interfaces
-{
-    public interface IActorService : ICRUDService<Actor, ActorInsertRequest, ActorUpdateRequest, ActorSearchObject> { }
-}
